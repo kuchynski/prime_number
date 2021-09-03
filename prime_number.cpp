e0f9c1d6d35146dd7e2ebc68cfc355a9adcd96dc
kuchynski <kuchynskiandrei@gmail.com>
2021-09-03 10:36:27 +0300

    using auto type deduction

--- prime_number.cpp
+++ prime_number.cpp
@@ -13,26 +13,26 @@ class PrimeNumbers
 private:
 	std::vector<unsigned> results;
 public:
-	PrimeNumbers(const unsigned min, const unsigned max)
+	PrimeNumbers(const auto min, const auto max)
 	{
-		auto func_init = [](unsigned n) { return (n > 1)? n / 2 - 1 : 0;};
-		auto func_convert = [](unsigned n) { return n + n + 3;};
+		auto func_init = [](auto n) { return (n > 1)? n / 2 - 1 : 0;};
+		auto func_convert = [](auto n) { return n + n + 3;};
 
-		const unsigned m_min_value = func_init(min);
-		const unsigned m_max_value = func_init(max);
+		const auto m_min_value = func_init(min);
+		const auto m_max_value = func_init(max);
 		std::vector<char> data(m_max_value, 1);
 	
-		for(unsigned i = 0; i < sqrt(m_max_value / 2); i++) {
+		for(auto i = 0; i < sqrt(m_max_value / 2); i++) {
 			if(data[i]) {
-				const unsigned number = func_convert(i);
-				for(unsigned j = i + number; j < m_max_value; j += number) {
+				const auto number = func_convert(i);
+				for(auto j = i + number; j < m_max_value; j += number) {
 					data[j] = 0;
 				}
 			}
 		}
 		if(min < 3)
 			results.push_back(2); // 2 is a prime number we don't have to calculate
-		for(unsigned i = m_min_value; i < m_max_value; i++) {
+		for(auto i = m_min_value; i < m_max_value; i++) {
 			if(data[i]) {
 				results.push_back(func_convert(i));
 			}
@@ -41,12 +41,12 @@ public:
 	void PrintAll(std::ostream &stream) const
 	{
 		for_each(results.begin(), results.end(), 
-			[&stream](unsigned value) { stream << value << " "; });
+			[&stream](auto value) { stream << value << " "; });
 		stream << "\n";
 	}
 
-	int GetSize() const {return results.size();}
-	unsigned operator[](unsigned n) const { return (n < GetSize())? results[n] : 0;}
+	auto GetSize() const {return results.size();}
+	unsigned operator[](auto n) const { return (n < GetSize())? results[n] : 0;}
 };
 
 int main(void)
