efb8241b79a8aa2a578ce745c8588386fb7e0bef
kuchynski <kuchynskiandrei@gmail.com>
2024-03-26 00:30:19 +0100

    dynamic implementation was added

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 
 SRC_FILES_C = ./prime_number.c
 SRC_FILES_CPP = ./prime_number.cpp
+SRC_FILES_CPP_VECTOR = ./prime_number_vector.cpp
 
 c: $(SRC_FILES_C)
 	gcc -o prime_number $^
@@ -10,3 +11,6 @@ c: $(SRC_FILES_C)
 
 cpp: $(SRC_FILES_CPP)
 	g++ -std=c++2a -fconcepts -o prime_number_cpp $^
+
+cpp_vector: $(SRC_FILES_CPP_VECTOR)
+	g++ -std=c++2a -fconcepts -o prime_number_cpp_vector $^
--- prime_number_vector.cpp
+++ prime_number_vector.cpp
@@ -9,27 +9,28 @@
 #include <iterator>
 #include <memory>
 
-template <std::size_t min_value,  std::size_t max_value>
 class PrimeNumbers: public std::vector<unsigned long>
 {
 public:
-	PrimeNumbers()
+	const size_t min;
+	const size_t max;
+
+	PrimeNumbers(size_t min_value, size_t max_value): min(min_value), max(max_value)
 	{
 		if(min_value < max_value) {
 			constexpr auto func_init = [](const auto n) { return (n > 1)? n / 2 - 1 : 0;};
 			auto func_convert = [](const auto n) { return n + n + 3;};
-			constexpr auto start = func_init(min_value);
-			constexpr auto finish = func_init(max_value);
-			auto pdata = std::make_unique<std::array<bool, finish>>();
-			std::fill(pdata->begin(), pdata->end(), true);
+			auto start = func_init(min_value);
+			auto finish = func_init(max_value);
+			vector<char> data(finish);
+			std::fill(data.begin(), data.end(), 1);
 
-			std::for_each_n(pdata->begin(), sqrt(finish / 2),
-				[index = 0, &pdata, &func_convert](auto n) mutable {
+			std::for_each_n(data.begin(), sqrt(finish / 2),
+				[index = 0, &data, &func_convert](auto n) mutable {
 					if(n) {
 						const auto step = func_convert(index);
-						const auto iter_data_end = pdata->end();
-						for(auto it = pdata->begin() + index + step; it < iter_data_end; it += step) {
-							*it = false;	// it isn't a prime number
+						for(auto it = data.begin() + index + step; it < data.end(); it += step) {
+							*it = 0;	// it isn't a prime number
 						}
 					}
 					index++;
@@ -38,25 +39,22 @@ public:
 
 			if(min_value <= 2)
 				push_back(2); // 2 is a prime number we don't have to calculate
-			std::for_each(pdata->begin() + start, pdata->end(),
-				[&, index = start](const auto e) mutable{
+			std::for_each(data.begin() + start, data.end(),
+				[&](auto e) mutable{
 					if(e)
-						push_back(func_convert(index));
-					index++;
+						push_back(func_convert(start));
+					start++;
 				}
 			);
 		}
 	}
-
-	static constexpr auto min = min_value;
-	static constexpr auto max = max_value;
 };
 
 int main(void)
 {
 	const auto min_value = 1'234'567'890;
 	const auto max_value = 1'234'567'999;
-	const PrimeNumbers<min_value, max_value> primes;
+	const PrimeNumbers primes(min_value, max_value);
 
 	std::cout << "There are " << primes.size() << " prime numbers in range [" << primes.min << ":" << primes.max << ")" << std::endl;
 	std::copy(primes.begin(), primes.end(), std::ostream_iterator<unsigned long>(std::cout, " "));
